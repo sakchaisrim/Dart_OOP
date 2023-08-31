1ff4d4cad3248ef14d0aff2c54147924c643c73a
sakchaisrim <sakchaisrim@gmail.com>
2023-08-31 15:01:48 +0700

    123

---
+++ Employee.dart
@@ -0,0 +1,21 @@
+void main() {
+  var emp1 = Employee("fern", 19, "LP");
+  emp1.display();
+}
+
+class Employee{
+  var name, age, address;
+
+  // Constructor method
+  Employee(name, age, address){
+    this.name = name;
+    this.age = age;
+    this.address = address;
+  }
+  display(){
+    print(this.name);
+    print(this.age);
+    print(this.address);
+  }
+
+}
--- Human.dart
+++ Human.dart
@@ -1,17 +1,16 @@
 void main() {
   var max = Human();
-  var fern = Human();
   max.id = 101;
   max.name = "max";
   max.age = 20;
+  print("${max.id} and ${max.name} and ${max.age}");
+  max.eat();
+  
+  var fern = Human();
   fern.id = 102;
   fern.name = "fern";
   fern.age = 19;
-  print("${max.id}");
-  print("${max.name}");
-  print("${max.age}");
   print("${fern.id} and ${fern.name} and ${fern.age}");
-  max.eat();
   fern.walk();
 }
 
@@ -23,13 +22,13 @@ class Human{
 
   // method
   void walk(){
-    print("${this.name} is walking now");
+    print("${this.name} is ${this.age} walking now");
   }
   void eat(){
-    print("${this.name} is eating now");
+    print("${this.name} is ${this.age} eating now");
   }
   void sleep(){
-    print("${this.name} is sleep now");
+    print("${this.name} is ${this.age} sleep now");
   }
 }
 
