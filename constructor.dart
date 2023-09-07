9cf8dad83c5bda2564dcb8abdc6e87d604a9348a
sakchaisrim <sakchaisrim@gmail.com>
2023-09-07 21:00:54 +0700

    1234

--- constructor.dart
+++ constructor.dart
@@ -5,18 +5,24 @@ class Employee {
   String? address;
 
   // Constructor method non parameter
-  Employee(){     
-    this.name;
-    this.age;
-    this.address;
-  }
+  // Employee(){     
+  //   this.name;
+  //   this.age;
+  //   this.address;
+  // }
+
+  // Constructor method name parameter
+  Employee({this.name, this.age, this.address});
+
   void display(){
     print(this.name);
   }
 }
 
 void main(){
-  var emp1 = Employee();
-  emp1.name;
-  emp1.display();
+  // var emp1 = Employee();
+  var emp1 = Employee(name: 'Jonn', age: 19, address:'bangkok');  //insatnce
+  // emp1.name = "Tony";
+  // emp1.display();
+  print("My name is ${emp1.name}. I'm ${emp1.age} year old. I live in ${emp1.address}.");
 }
